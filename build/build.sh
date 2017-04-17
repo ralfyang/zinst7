666bccddf51305bd52b91cc923548cc2d222fd8f
Ralf Yang <goody80762@gmail.com>
2017-04-17 10:07:07 +0100

    update for build

---
+++ build/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+src_org="./main"
+target_file="./zinst"
+endline=`wc -l $src_org| awk '{print $1}'`
+count=1
+	rm -f zinst;touch zinst
+	while [[ $count -le $endline ]];do	
+		if [[ `(echo $(head  -$count $src_org  | tail -n 1 |egrep 'source \.\/src'))` != "" ]];then
+			Exec=`echo $(head  -$count $src_org  | tail -n 1 | sed -e 's/source /cat /g')`
+			$Exec |sed '/^\#\!\/bin\/bash/d'  >> $target_file
+		else
+			head  -$count $src_org  | tail -n 1 >> $target_file
+		fi
+	
+	let count=$count+1
+	done
+
+chmod 755 $target_file
