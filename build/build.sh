6b3cab71ce086bdd4640c4679528457f67176edd
Ralf Yang <goody80762@gmail.com>
2017-04-18 02:03:39 +0100

    update for build progresive bar

--- build/build.sh
+++ build/build.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
+BARR="==================================================================================================="
+Barr="---------------------------------------------------------------------------------------------------"
+pstr="[==========================================]"
 
 src_org="./main"
 target_file="../zinst"
+
+## All source scan
+all_src=(`egrep 'source \.\/src' ./main | awk '{print $2}'`)
+
+## Define Last line number
 endline=`wc -l $src_org| awk '{print $1}'`
-count=1
+	## Purge the old artifact
 	rm -f $target_file;touch $target_file
+
+	## Start loop
+	echo "$BARR"
+	echo " Building..."
+	echo "$Barr"
+	count=1
+	Cnt=1
 	while [[ $count -le $endline ]];do	
-		if [[ `(echo $(head  -$count $src_org  | tail -n 1 |egrep 'source \.\/src'))` != "" ]];then
-			Exec=`echo $(head  -$count $src_org  | tail -n 1 | sed -e 's/source /cat /g')`
+		if [[ `(echo $(awk "NR == $count" $src_org |egrep 'source \.\/src'))` != "" ]];then
+			Exec=`echo $(awk "NR == $count" $src_org | sed -e 's/source /cat /g')`
 			$Exec |sed '/^\#\!\/bin\/bash/d'  >> $target_file
+			## Part of Progress bar
+			pd=$(( $Cnt * 45 / ${#all_src[@]} ))
+				if [[ $Cnt = ${#all_src[@]} ]];then
+					Pkg_picklist=" - Build has been completed -"
+				else
+					Pkg_picklist="${all_src[$Cnt]}"
+				fi
+			printf "\r%-2s %-40s %3s %3d.%1d%% %.${pd}s" '|' "$Pkg_picklist" '|' $(( $Cnt * 100 / ${#all_src[@]} )) $(( ($Cnt * 1000 / ${#all_src[@]}) % 10 )) $pstr
+			let Cnt=$Cnt+1
 		else
-			head  -$count $src_org  | tail -n 1 >> $target_file
+			awk "NR == $count" $src_org >> $target_file
 		fi
 	
 	let count=$count+1
 	done
+## Line braker for Progress bar
+echo ""
+echo "$BARR"
 
+## Permission change
 chmod 755 $target_file
+
+
