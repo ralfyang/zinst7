17de57c27455055dc693daf475e471214d38a23a
root <ralfyang@code-post.com>
2017-06-19 04:27:20 +0100

    zinst command update for #24

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo zinst self-config ip=$BaseIP host=$BaseIP dir=/data
 sudo rm -Rf ./RootDirectorY
 
 cp ./base_package/* ./
-zinst i server_default_setting-1.3.0.zinst 
+zinst i server_default_setting -stable
 zinst i zinst_simple_server-1.0.4.zinst -same
 
 sudo cp -Rfv ./dist/rhel7/* /data/dist/rhel7/
